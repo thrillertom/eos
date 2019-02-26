0cb32378d9de0c4bb40818a123f92a6a3651bb83
thrillertom <thrillertom@gmail.com>
2019-02-26 11:16:17 +0800

    include filter-on, filter-out

--- plugins/kinesis_plugin/kinesis_plugin.cpp
+++ plugins/kinesis_plugin/kinesis_plugin.cpp
@@ -17,11 +17,12 @@
 #include <fc/utf8.hpp>
 #include <fc/variant.hpp>
 
+#include <boost/algorithm/string.hpp>
 #include <boost/chrono.hpp>
 #include <boost/signals2/connection.hpp>
-#include <boost/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
 #include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
 
 #include <queue>
 #include <string>
@@ -42,6 +43,24 @@ namespace eosio {
 
   static appbase::abstract_plugin& _kinesis_plugin = app().register_plugin<kinesis_plugin>();
   using kinesis_producer_ptr = std::shared_ptr<class kinesis_producer>;
+
+  struct filter_entry {
+    name receiver;
+    name action;
+    name actor;
+
+    friend bool operator<(const filter_entry &a, const filter_entry &b) {
+      return std::tie(a.receiver, a.action, a.actor) <
+             std::tie(b.receiver, b.action, b.actor);
+    }
+
+    //            receiver          action       actor
+    bool match(const name &rr, const name &an, const name &ar) const {
+      return (receiver.value == 0 || receiver == rr) &&
+             (action.value == 0 || action == an) &&
+             (actor.value == 0 || actor == ar);
+    }
+  };
 
   class kinesis_plugin_impl {
   public:
@@ -83,10 +102,17 @@ namespace eosio {
 
     void add_action_trace(const chain::action_trace &, const chain::transaction_trace_ptr &);
 
+    bool
+    filter_include(const account_name &receiver, const action_name &act_name,
+                   const vector<chain::permission_level> &authorization) const;
+
     void init(int thread_number = 32);
 
     uint32_t start_block_num = 0;
     bool start_block_reached = false;
+    bool filter_on_star = true;
+    std::set<filter_entry> filter_on;
+    std::set<filter_entry> filter_out;
 
     size_t queue_size = 10000;
     std::deque<chain::transaction_metadata_ptr> transaction_metadata_queue;
@@ -132,6 +158,49 @@ namespace eosio {
       condition.notify_one();
     }
 
+  }
+
+  bool kinesis_plugin_impl::filter_include( const account_name& receiver, const action_name& act_name,
+                                            const vector<chain::permission_level>& authorization ) const
+  {
+    bool include = false;
+    if( filter_on_star ) {
+        include = true;
+    } else {
+        auto itr = std::find_if( filter_on.cbegin(), filter_on.cend(), [&receiver, &act_name]( const auto& filter ) {
+          return filter.match( receiver, act_name, 0 );
+        } );
+        if( itr != filter_on.cend() ) {
+          include = true;
+        } else {
+          for( const auto& a : authorization ) {
+              auto itr = std::find_if( filter_on.cbegin(), filter_on.cend(), [&receiver, &act_name, &a]( const auto& filter ) {
+                return filter.match( receiver, act_name, a.actor );
+              } );
+              if( itr != filter_on.cend() ) {
+                include = true;
+                break;
+              }
+          }
+        }
+    }
+
+    if( !include ) { return false; }
+    if( filter_out.empty() ) { return true; }
+
+    auto itr = std::find_if( filter_out.cbegin(), filter_out.cend(), [&receiver, &act_name]( const auto& filter ) {
+        return filter.match( receiver, act_name, 0 );
+    } );
+    if( itr != filter_out.cend() ) { return false; }
+
+    for( const auto& a : authorization ) {
+        auto itr = std::find_if( filter_out.cbegin(), filter_out.cend(), [&receiver, &act_name, &a]( const auto& filter ) {
+          return filter.match( receiver, act_name, a.actor );
+        } );
+        if( itr != filter_out.cend() ) { return false; }
+    }
+
+    return true;
   }
 
   void kinesis_plugin_impl::accepted_transaction(const chain::transaction_metadata_ptr &t) {
@@ -360,23 +429,34 @@ namespace eosio {
   }
 
   void kinesis_plugin_impl::add_action_trace(const chain::action_trace& atrace, const chain::transaction_trace_ptr& t) {
-    const chain::base_action_trace& base = atrace; // without inline action traces
 
-    fc::variant pretty_output;
-    abi_serializer::to_variant(base,
-                               pretty_output,
-                               make_resolver(chain_plug->chain(), abi_serializer_max_time),
-                               abi_serializer_max_time);
-    string json;
-    if( t->receipt.valid() ) {
-      json = fc::json::to_string(fc::mutable_variant_object(pretty_output.get_object())
-                                        ("trx_status", std::string( t->receipt->status )));
-    } else {
-      json = fc::json::to_string(fc::mutable_variant_object(pretty_output.get_object()));
+    const bool in_filter =
+        start_block_reached &&
+        filter_include(atrace.receipt.receiver, atrace.act.name,
+                       atrace.act.authorization);
+
+    if (start_block_reached && in_filter) {
+      const chain::base_action_trace &base =
+          atrace; // without inline action traces
+
+      fc::variant pretty_output;
+      abi_serializer::to_variant(
+          base, pretty_output,
+          make_resolver(chain_plug->chain(), abi_serializer_max_time),
+          abi_serializer_max_time);
+      string json;
+      if (t->receipt.valid()) {
+        json = fc::json::to_string(
+            fc::mutable_variant_object(pretty_output.get_object())(
+                "trx_status", std::string(t->receipt->status)));
+      } else {
+        json = fc::json::to_string(
+            fc::mutable_variant_object(pretty_output.get_object()));
+      }
+      json = fc::prune_invalid_utf8(json);
+      // ilog("pushing action trace: " + json);
+      producer->kinesis_sendmsg(json);
     }
-    json = fc::prune_invalid_utf8( json );
-    ilog("pushing action trace: " + json);
-    producer->kinesis_sendmsg(json);
 
     for( const auto& iline_atrace : atrace.inline_traces ) {
       add_action_trace(iline_atrace, t);
@@ -491,14 +571,21 @@ namespace eosio {
   }
 
   void kinesis_plugin::set_program_options(options_description &cli, options_description &cfg) {
-    cfg.add_options()
-      ("aws-region-name", bpo::value<std::string>(),
-       "AWS region name in string, e.g. 'ap-northeast-1'")
-      ("aws-stream-name", bpo::value<std::string>(),
-       "The stream name for AWS kinesis.")
-      ("kinesis-block-start", bpo::value<uint32_t>()->default_value(0),
-       "If specified then only abi data pushed to kinesis until specified block is reached.")
-      ;
+    cfg.add_options()("aws-region-name", bpo::value<std::string>(),
+                      "AWS region name in string, e.g. 'ap-northeast-1'")(
+        "aws-stream-name", bpo::value<std::string>(),
+        "The stream name for AWS kinesis.")(
+        "kinesis-block-start", bpo::value<uint32_t>()->default_value(0),
+        "If specified then only abi data pushed to kinesis until specified "
+        "block is reached.")
+        ("kinesis-filter-on", bpo::value<vector<string>>()->composing(),
+         "Track actions which match receiver:action:actor. Receiver, Action, & "
+         "Actor may be blank to include all. i.e. eosio:: or :transfer:  Use * "
+         "or leave unspecified to include all.")(
+            "kinesis-filter-out", bpo::value<vector<string>>()->composing(),
+            "Do not track actions which match receiver:action:actor. Receiver, "
+            "Action, & Actor may be blank to exclude all.");
+    ;
   }
 
   void kinesis_plugin::plugin_initialize(const variables_map &options) {
@@ -528,6 +615,36 @@ namespace eosio {
         my->start_block_reached = true;
       }
 
+      if (options.count("kinesis-filter-on")) {
+        auto fo = options.at("kinesis-filter-on").as<vector<string>>();
+        my->filter_on_star = false;
+        for (auto &s : fo) {
+          if (s == "*") {
+            my->filter_on_star = true;
+            break;
+          }
+          std::vector<std::string> v;
+          boost::split(v, s, boost::is_any_of(":"));
+          EOS_ASSERT(v.size() == 3, fc::invalid_arg_exception,
+                     "Invalid value ${s} for --kinesis-filter-on", ("s", s));
+          filter_entry fe{v[0], v[1], v[2]};
+          my->filter_on.insert(fe);
+        }
+      } else {
+        my->filter_on_star = true;
+      }
+
+      if (options.count("kinesis-filter-out")) {
+        auto fo = options.at("kinesis-filter-out").as<vector<string>>();
+        for (auto &s : fo) {
+          std::vector<std::string> v;
+          boost::split(v, s, boost::is_any_of(":"));
+          EOS_ASSERT(v.size() == 3, fc::invalid_arg_exception,
+                     "Invalid value ${s} for --kinesis-filter-out", ("s", s));
+          filter_entry fe{v[0], v[1], v[2]};
+          my->filter_out.insert(fe);
+        }
+      }
       // hook up to signals on controller
       //chain_plugin* chain_plug = app().find_plugiin<chain_plugin>();
       my->chain_plug = app().find_plugin<chain_plugin>();

cece2491f5273f56bd5b0a126b61a68a03c26cbb
thrillertom <thrillertom@gmail.com>
2019-03-15 19:58:45 +0800

    process accepted blocks

--- plugins/kinesis_plugin/kinesis_plugin.cpp
+++ plugins/kinesis_plugin/kinesis_plugin.cpp
@@ -390,6 +390,7 @@ namespace eosio {
 
   void kinesis_plugin_impl::process_accepted_block(const chain::block_state_ptr &bs) {
     try {
+      ilog("current block num: {num}", ("num", bs->block_num));
       if (!start_block_reached) {
         if (bs->block_num >= start_block_num) {
           start_block_reached = true;
@@ -652,9 +653,9 @@ namespace eosio {
       auto &chain = my->chain_plug->chain();
       my->chain_id.emplace(chain.get_chain_id());
 
-      // my->accepted_block_connection.emplace(chain.accepted_block.connect( [&]( const chain::block_state_ptr& bs ) {
-      //                                                                        my->accepted_block(bs);
-      //                                                                      }));
+      my->accepted_block_connection.emplace(chain.accepted_block.connect( [&]( const chain::block_state_ptr& bs ) {
+                                                                             my->accepted_block(bs);
+                                                                           }));
 
       // my->irreversible_block_connection.emplace(chain.irreversible_block.connect([&](const chain::block_state_ptr &bs) {
       //                                                                              my->applied_irreversible_block(bs);
